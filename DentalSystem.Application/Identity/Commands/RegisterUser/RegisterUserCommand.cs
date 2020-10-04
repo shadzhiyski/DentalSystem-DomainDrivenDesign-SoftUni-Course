d19f605f13d168ff0344ab828f1eea43ddeeb52e
stoian2662 <stoian2662@gmail.com>
2020-10-04 14:51:01 +0300

    ChangePasswordCommand, RegisterUserCommand and RegisterUserCommandValidator added.

---
+++ DentalSystem.Application/Identity/Commands/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,60 @@
+﻿namespace DentalSystem.Application.Identity.Commands.CreateUser
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Common;
+    using DentalSystem.Application.Core.AppointmentScheduling;
+    using DentalSystem.Domain.ClientPatientManagement.Builders;
+    using MediatR;
+
+    public class RegisterUserCommand : UserInputModel, IRequest<Result>
+    {
+        public string FirstName { get; set; } = default!;
+
+        public string LastName { get; set; } = default!;
+
+        public string PhoneNumber { get; set; } = default!;
+
+        public class CreateUserCommandHandler : IRequestHandler<RegisterUserCommand, Result>
+        {
+            private readonly IIdentity _identity;
+            private readonly IClientBuilder _clientBuilder;
+            private readonly IClientPatientManagementRepository _clientPatientManagementRepository;
+
+            public CreateUserCommandHandler(
+                IIdentity identity, 
+                IClientBuilder clientBuilder, 
+                IClientPatientManagementRepository clientPatientManagementRepository)
+            {
+                _identity = identity;
+                _clientBuilder = clientBuilder;
+                _clientPatientManagementRepository = clientPatientManagementRepository;
+            }
+
+            public async Task<Result> Handle(
+                RegisterUserCommand request,
+                CancellationToken cancellationToken)
+            {
+                var result = await _identity.RegisterAsync(request);
+
+                if (!result.Succeeded)
+                {
+                    return result;
+                }
+
+                var user = result.Data;
+
+                var client = _clientBuilder
+                    .WithNames(request.FirstName, request.LastName)
+                    .WithPhoneNumber(request.PhoneNumber)
+                    .Build();
+
+                user.BecomeClient(client);
+
+                await _clientPatientManagementRepository.SaveAsync(client, cancellationToken);
+
+                return result;
+            }
+        }
+    }
+}
---
+++ DentalSystem.Application/Identity/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿namespace DentalSystem.Application.Identity.Commands.CreateUser
+{
+    using FluentValidation;
+    using static Domain.Common.Models.ModelConstants.Common;
+    using static Domain.Common.Models.ModelConstants.PhoneNumber;
+
+    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(u => u.Email)
+                .MinimumLength(MinEmailLength)
+                .MaximumLength(MaxEmailLength)
+                .EmailAddress()
+                .NotEmpty();
+
+            RuleFor(u => u.Password)
+                .MaximumLength(MaxNameLength)
+                .NotEmpty();
+
+            RuleFor(u => u.FirstName)
+                .MinimumLength(MinNameLength)
+                .MaximumLength(MaxNameLength)
+                .NotEmpty();
+
+            RuleFor(u => u.PhoneNumber)
+                .MinimumLength(MinPhoneNumberLength)
+                .MaximumLength(MaxPhoneNumberLength)
+                .Matches(PhoneNumberRegularExpression)
+                .NotEmpty();
+        }
+    }
+}
