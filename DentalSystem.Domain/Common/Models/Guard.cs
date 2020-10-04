c66abc75f2e502207ac6abcfde5b29f8ac76cf2e
stoian2662 <stoian2662@gmail.com>
2020-10-04 14:44:04 +0300

    Guard added.

---
+++ DentalSystem.Domain/Common/Models/Guard.cs
@@ -0,0 +1,42 @@
+﻿namespace CarRentalSystem.Domain.Common.Models
+{
+    using DentalSystem.Domain.Common;
+
+    public static class Guard
+    {
+        public static void AgainstEmptyString<TException>(string value, string name = "Value")
+            where TException : BaseDomainException, new()
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                return;
+            }
+
+            ThrowException<TException>($"{name} cannot be null ot empty.");
+        }
+
+        public static void ForStringLength<TException>(string value, int minLength, int maxLength, string name = "Value")
+            where TException : BaseDomainException, new()
+        {
+            AgainstEmptyString<TException>(value, name);
+
+            if (minLength <= value.Length && value.Length <= maxLength)
+            {
+                return;
+            }
+
+            ThrowException<TException>($"{name} must have between {minLength} and {maxLength} symbols.");
+        }
+
+        private static void ThrowException<TException>(string message)
+            where TException : BaseDomainException, new()
+        {
+            var exception = new TException
+            {
+                Error = message
+            };
+
+            throw exception;
+        }
+    }
+}
