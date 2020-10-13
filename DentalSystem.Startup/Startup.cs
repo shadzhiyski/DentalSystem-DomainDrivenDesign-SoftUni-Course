8e33bb759430944a5b32c60f8ec3d94f9c00b139
stoian2662 <stoian2662@gmail.com>
2020-10-13 17:14:11 +0300

    WebConfiguration added and applied to Startup.

--- DentalSystem.Startup/Startup.cs
+++ DentalSystem.Startup/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using DentalSystem.Web;
 
 namespace DentalSystem.Startup
 {
@@ -28,7 +29,7 @@ namespace DentalSystem.Startup
         {
             services
                 .AddInfrastructure(this.Configuration)
-                .AddControllers();
+                .AddWebComponents();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
