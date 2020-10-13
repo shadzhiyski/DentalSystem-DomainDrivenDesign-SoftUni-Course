c2b67f70d50de42022c0dafa20e03330b92c9cf5
stoian2662 <stoian2662@gmail.com>
2020-10-13 09:56:13 +0300

    IdentityController added.

---
+++ DentalSystem.Web/Features/IdentityController.cs
@@ -0,0 +1,32 @@
+﻿namespace DentalSystem.Web.Features
+{
+    using System.Threading.Tasks;
+    using Application.Identity.Commands.ChangePassword;
+    using Application.Identity.Commands.CreateUser;
+    using Application.Identity.Commands.LoginUser;
+    using DentalSystem.Web.Common;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class IdentityController : ApiController
+    {
+        [HttpPost]
+        [Route(nameof(Register))]
+        public async Task<ActionResult> Register(
+            RegisterUserCommand command)
+            => await this.Send(command);
+
+        [HttpPost]
+        [Route(nameof(Login))]
+        public async Task<ActionResult<LoginOutputModel>> Login(
+            LoginUserCommand command)
+            => await this.Send(command);
+
+        [HttpPut]
+        [Authorize]
+        [Route(nameof(ChangePassword))]
+        public async Task<ActionResult> ChangePassword(
+            ChangePasswordCommand command)
+            => await this.Send(command);
+    }
+}
