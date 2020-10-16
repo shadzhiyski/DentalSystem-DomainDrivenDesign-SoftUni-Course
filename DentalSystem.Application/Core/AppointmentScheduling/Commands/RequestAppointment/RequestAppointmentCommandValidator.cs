9aaf3bc5027d85d5401d6c70154390cdb84a32ae
stoian2662 <stoian2662@gmail.com>
2020-10-16 22:40:10 +0300

    RequestAppointmentCommandValidator added.

---
+++ DentalSystem.Application/Core/AppointmentScheduling/Commands/RequestAppointment/RequestAppointmentCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿namespace DentalSystem.Application.Core.AppointmentScheduling.Commands.RequestAppointment
+{
+    using DentalSystem.Domain.Core.AppointmentScheduling.Models;
+    using FluentValidation;
+
+    public class RequestAppointmentCommandValidator : AbstractValidator<AppointmentInputModel>
+    {
+        public RequestAppointmentCommandValidator()
+        {
+            RuleFor(m => m.Treatment)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(model => model.Treatment)
+                .IsEnumName(typeof(TreatmentType));
+
+            RuleFor(m => m.DentalTeam)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(m => m.Start)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(m => m.End)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(model => model.End)
+                .GreaterThan(model => model.Start);
+        }
+    }
+}
