bd599f6b557357d9d8af2b61943c2578a40376df
stoian2662 <stoian2662@gmail.com>
2020-10-17 00:01:02 +0300

    InfrastructureConfigurationSpecs added.

---
+++ DentalSystem.Infrastructure/InfrastructureConfiguration.Specs.cs
@@ -0,0 +1,39 @@
+﻿namespace DentalSystem.Infrastructure
+{
+    using System;
+    using System.Reflection;
+    using AutoMapper;
+    using Common.Persistence;
+    using DentalSystem.Application.Core.AppointmentScheduling;
+    using DentalSystem.Infrastructure.Core.AppointmentScheduling;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    public class InfrastructureConfigurationSpecs
+    {
+        [Fact]
+        public void AddRepositoriesShouldRegisterRepositories()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection()
+                .AddDbContext<CoreDbContext>(opts => opts
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString()))
+                .AddTransient(_ => A.Fake<IAppointmentSchedulingDbContext>());
+
+            // Act
+            var services = serviceCollection
+                .AddAutoMapper(Assembly.GetExecutingAssembly())
+                .AddRepositories()
+                .BuildServiceProvider();
+
+            // Assert
+            services
+                .GetService<IAppointmentSchedulingRepository>()
+                .Should()
+                .NotBeNull();
+        }
+    }
+}
