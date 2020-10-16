b37a066e9dad33cbf039fef009f4c432e4f8aec7
stoian2662 <stoian2662@gmail.com>
2020-10-16 23:38:39 +0300

    ValueObjectSpecs added.

---
+++ DentalSystem.Domain/Common/Models/ValueObject.Specs.cs
@@ -0,0 +1,36 @@
+﻿namespace DentalSystem.Domain.Common.Models
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class ValueObjectSpecs
+    {
+        [Fact]
+        public void ValueObjectsWithEqualPropertiesShouldBeEqual()
+        {
+            // Arrange
+            var first = new FullName("John", "Doe");
+            var second = new FullName("John", "Doe");
+
+            // Act
+            var result = first == second;
+
+            // Arrange
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ValueObjectsWithDifferentPropertiesShouldNotBeEqual()
+        {
+            // Arrange
+            var first = new FullName("John", "Doe");
+            var second = new FullName("Jane", "Doe");
+
+            // Act
+            var result = first == second;
+
+            // Arrange
+            result.Should().BeFalse();
+        }
+    }
+}
