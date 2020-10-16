c01672275e5b7036e3a6944fac92aab715da8dd4
stoian2662 <stoian2662@gmail.com>
2020-10-16 22:27:25 +0300

    UserId added.

--- DentalSystem.Domain/Core/AppointmentScheduling/Models/Client.cs
+++ DentalSystem.Domain/Core/AppointmentScheduling/Models/Client.cs
@@ -6,19 +6,23 @@ namespace DentalSystem.Domain.Core.AppointmentScheduling.Models
     public class Client : Entity<Guid>
     {
         internal Client()
-            : this(default!, default!)
+            : this(default!, default!, default!)
         { }
 
         internal Client(
+            string userId,
             FullName fullName,
             Patient patient)
         {
+            UserId = userId;
             FullName = fullName;
             Patient = patient;
         }
 
+        public string UserId { get; private set; }
+
         public FullName FullName { get; private set; }
-        
+
         public Patient Patient { get; private set; }
     }
 }
