4c0a79d59d911bdfd12808752e3bfa451a75701e
stoian2662 <stoian2662@gmail.com>
2020-10-16 23:56:15 +0300

    DomainConfigurationSpecs added.

---
+++ DentalSystem.Domain/DomainConfiguration.Specs.cs
@@ -0,0 +1,35 @@
+﻿namespace DentalSystem.Domain
+{
+    using DentalSystem.Domain;
+    using DentalSystem.Domain.Core.AppointmentScheduling.Builders;
+    using DentalSystem.Domain.Core.ClientPatientManagement.Builders;
+    using FluentAssertions;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    public class DomainConfigurationSpecs
+    {
+        [Fact]
+        public void AddDomainShouldRegisterFactories()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            var services = serviceCollection
+                .AddDomain()
+                .BuildServiceProvider();
+
+            // Assert
+            services
+                .GetService<IAppointmentBuilder>()
+                .Should()
+                .NotBeNull();
+
+            services
+                .GetService<IClientBuilder>()
+                .Should()
+                .NotBeNull();
+        }
+    }
+}
