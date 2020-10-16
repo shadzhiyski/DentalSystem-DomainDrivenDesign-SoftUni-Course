f0bc5fa188feee28ff93a960ab2f17d22ab8033b
stoian2662 <stoian2662@gmail.com>
2020-10-17 00:13:54 +0300

    RegisterUserCommandFakes added.

---
+++ DentalSystem.Application/Identity/Commands/RegisterUser/RegisterUserCommand.Fakes.cs
@@ -0,0 +1,19 @@
+﻿namespace DentalSystem.Application.Identity.Commands.CreateUser
+{
+    using Bogus;
+
+    public class RegisterUserCommandFakes
+    {
+        public static class Data
+        {
+            public static RegisterUserCommand GetCommand()
+                => new Faker<RegisterUserCommand>()
+                    .RuleFor(u => u.Email, f => f.Internet.Email())
+                    .RuleFor(u => u.Password, f => f.Lorem.Letter(10))
+                    .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+                    .RuleFor(u => u.LastName, f => f.Name.LastName())
+                    .RuleFor(u => u.PhoneNumber, f => f.Phone.PhoneNumber("+#######"))
+                    .Generate();
+        }
+    }
+}
