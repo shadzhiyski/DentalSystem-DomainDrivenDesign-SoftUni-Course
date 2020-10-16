e82055879b4a1b42fa54dfd0f286787a469c78da
stoian2662 <stoian2662@gmail.com>
2020-10-16 17:18:47 +0300

    Inheritance of IRepository added.

--- DentalSystem.Application/Core/ClientPatientManagement/IClientPatientManagementRepository.cs
+++ DentalSystem.Application/Core/ClientPatientManagement/IClientPatientManagementRepository.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using DentalSystem.Application.Common.Contracts;
 using DentalSystem.Domain.ClientPatientManagement.Models;
 
 namespace DentalSystem.Application.Core.AppointmentScheduling
 {
-    public interface IClientPatientManagementRepository
+    public interface IClientPatientManagementRepository : IRepository<Client>
     {
         Task<Guid> GetClientId(string userId, CancellationToken cancellationToken = default);
-
-        Task SaveAsync(Client client, CancellationToken cancellationToken);    
     }
 }
