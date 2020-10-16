07804670c9a4fbeb1a83dbc1dd90b09cdf8fda49
stoian2662 <stoian2662@gmail.com>
2020-10-16 17:19:13 +0300

    CoreDbContext added.

---
+++ DentalSystem.Infrastructure/Common/Persistence/CoreDbContext.cs
@@ -0,0 +1,68 @@
+using DentalSystem.Domain.AppointmentScheduling.Models;
+using DentalSystem.Infrastructure.Core.AppointmentScheduling;
+using DentalSystem.Infrastructure.Core.ClientPatientManagement;
+using DentalSystem.Infrastructure.Core.Payments;
+using DentalSystem.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DentalSystem.Infrastructure.Common.Persistence
+{
+    public class CoreDbContext : IdentityDbContext<User>,
+        IAppointmentSchedulingDbContext,
+        IClientPatientManagementDbContext,
+        IPaymentsDbContext
+    {
+        public CoreDbContext(DbContextOptions<CoreDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new Identity.Configuration.UserConfiguration());
+
+            modelBuilder.ApplyConfiguration(new Core.AppointmentScheduling.Configuration.AppointmentConfiguration());
+            modelBuilder.ApplyConfiguration(new Core.AppointmentScheduling.Configuration.ClientConfiguration());
+            modelBuilder.ApplyConfiguration(new Core.AppointmentScheduling.Configuration.DentalWorkerConfiguration());
+            modelBuilder.ApplyConfiguration(new Core.AppointmentScheduling.Configuration.DentalTeamConfiguration());
+            modelBuilder.ApplyConfiguration(new Core.AppointmentScheduling.Configuration.PatientConfiguration());
+            modelBuilder.ApplyConfiguration(new Core.AppointmentScheduling.Configuration.RoomConfiguration());
+
+            modelBuilder.ApplyConfiguration(new Core.ClientPatientManagement.Configuration.ClientConfiguration());
+            modelBuilder.ApplyConfiguration(new Core.ClientPatientManagement.Configuration.PatientConfiguration());
+
+            modelBuilder.ApplyConfiguration(new Core.Payments.Configuration.ClientConfiguration());
+            modelBuilder.ApplyConfiguration(new Core.Payments.Configuration.CreditCardConfiguration());
+            modelBuilder.ApplyConfiguration(new Core.Payments.Configuration.PaymentConfiguration());
+        }
+
+        DbSet<Appointment> IAppointmentSchedulingDbContext.Appointments => Set<Appointment>();
+
+        DbSet<Client> IAppointmentSchedulingDbContext.Clients => Set<Client>();
+
+        DbSet<DentalTeam> IAppointmentSchedulingDbContext.DentalTeams => Set<DentalTeam>();
+
+        DbSet<DentalWorker> IAppointmentSchedulingDbContext.DentalWorkers => Set<DentalWorker>();
+
+        DbSet<Patient> IAppointmentSchedulingDbContext.Patients => Set<Patient>();
+
+        DbSet<Room> IAppointmentSchedulingDbContext.Rooms => Set<Room>();
+
+        DbSet<Domain.ClientPatientManagement.Models.Client> IClientPatientManagementDbContext.Clients
+            => Set<Domain.ClientPatientManagement.Models.Client>();
+
+        DbSet<Domain.ClientPatientManagement.Models.Patient> IClientPatientManagementDbContext.Patients
+            => Set<Domain.ClientPatientManagement.Models.Patient>();
+
+        DbSet<Domain.Payments.Models.Client> IPaymentsDbContext.Clients
+            => Set<Domain.Payments.Models.Client>();
+
+        DbSet<Domain.Payments.Models.CreditCard> IPaymentsDbContext.CreditCards
+            => Set<Domain.Payments.Models.CreditCard>();
+
+        DbSet<Domain.Payments.Models.Payment> IPaymentsDbContext.Payments
+            => Set<Domain.Payments.Models.Payment>();
+    }
+}
